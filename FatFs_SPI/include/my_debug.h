a41e263ef6dbbdceb8c050bd1b34a2f31feb1aff
James Smith <jmsmith86@gmail.com>
2023-04-06 07:19:05 -0600

    Disable DBG_PRINTF

--- FatFs_SPI/include/my_debug.h
+++ FatFs_SPI/include/my_debug.h
@@ -1,14 +1,14 @@
 /* my_debug.h
 Copyright 2021 Carl John Kugler III
 
-Licensed under the Apache License, Version 2.0 (the License); you may not use 
-this file except in compliance with the License. You may obtain a copy of the 
+Licensed under the Apache License, Version 2.0 (the License); you may not use
+this file except in compliance with the License. You may obtain a copy of the
 License at
 
-   http://www.apache.org/licenses/LICENSE-2.0 
-Unless required by applicable law or agreed to in writing, software distributed 
-under the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR 
-CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 #pragma once
@@ -29,11 +29,11 @@ extern "C" {
 #endif
 
 
-//#if defined(DEBUG) && !defined(NDEBUG)
+#if defined(DEBUG) && !defined(NDEBUG)
 #define DBG_PRINTF my_printf
-//#else
-//#define DBG_PRINTF(fmt, args...) /* Don't do anything in release builds*/
-//#endif
+#else
+#define DBG_PRINTF(fmt, args...) /* Don't do anything in release builds*/
+#endif
 
 #define myASSERT(__e) \
     ((__e) ? (void)0 : my_assert_func(__FILE__, __LINE__, __func__, #__e))
